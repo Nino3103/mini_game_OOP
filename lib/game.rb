6039bfe82b63bf5ddbb2a050ad9ab74472da1bcd
Nino3103 <jeangilardo@gmail.com>
2019-04-17 08:54:43 +0200

    test

--- lib/game.rb
+++ lib/game.rb
@@ -7,14 +7,12 @@ class Game
   def initialize(name, number_of_ennemies = 4)
     @human_player = HumanPlayer.new(name)
     @ennemies = Array.new(number_of_ennemies){|i|"creature_#{i}"}.map {|creature| Player.new("#{creature}")}
-    # @players_left = number_enemies
-    # @enemies_tot = number_enemies
-    # @enemies_in_sight = []
+
   end
 
   def kill_player(player)
     @ennemies.delete_if{|player| player.name == player.to_s}
   end
 
-#a_of_obj.delete_if {|obj| obj.a == 33}
+
 end
